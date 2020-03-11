a08deadec7c4d3d4da98b495c913860e1c2aeab1
mortizo <mortizo@ups.edu.ec>
2020-03-11 04:31:32 +0100

    Smart contract is interacting with web3 and react

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Navbar from 'react-bootstrap/Navbar';
-import getWeb3 from "./Web3";
+import getWeb3 from './Web3';
+import Contract001 from './Contract001';
 
 class App extends Component {
 
@@ -12,6 +13,7 @@ class App extends Component {
     super(props)
     this.state = {
       account:'',
+      contract001:null,
       textValue01:'',
       data:'void'
     }
@@ -19,14 +21,23 @@ class App extends Component {
 
   async componentWillMount(){
     this.web3 = await getWeb3();
-    const accounts = await this.web3.eth.getAccounts()
-    this.setState({account: accounts[0]})
+    const _accounts = await this.web3.eth.getAccounts()
+    const _datosRed = Contract001.networks[await this.web3.eth.net.getId()]
+    const _contract001 =  new this.web3.eth.Contract(Contract001.abi, _datosRed.address)
+    this.setState({
+      account: _accounts[0],
+      contract001: _contract001
+    })
+    
+  }
+
+  functionSetData = async (event) => {
+    await this.state.contract001.methods.setData(this.state.textValue01).send({from: this.state.account})
   }
 
   functionData = async (event) => {
-    this.state.data = this.state.textValue01
-    alert('The data was submitted: ' + this.state.data);
-    event.preventDefault();
+    this.state.data = await this.state.contract001.methods.data().call()
+    alert('The data was submitted: ' + this.state.data)
   }
 
   onChange(event) {
@@ -53,7 +64,9 @@ class App extends Component {
                   placeholder="Text Here !" 
                   value={this.state.textValue01}
                   onChange={this.onChange.bind(this)} />
-                <Button variant="primary" type="submit" onClick={this.functionData} >Send</Button> 
+                <Button variant="primary" type="submit" onClick={this.functionSetData} > Send </Button> 
+                <br></br>
+                <Button variant="primary" type="submit" onClick={this.functionData} > Data stored </Button> 
               </div>
             </main>
           </div>  
